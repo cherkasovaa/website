c1c899d955409a75bf5bfdee1042b9bf02a79954
cherkasovaa <alina.ch1202@gmail.com>
2025-06-05 00:51:48 +0300

    feat: add theme provider
    Add context and logic for switching system theme (light/dark)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,12 @@
 import type { FC } from 'react';
 
+import { ThemeProvider } from './providers/ThemeProvider';
 import { AppRouter } from './routes/AppRouter';
 
-const App: FC = () => <AppRouter />;
+const App: FC = () => (
+  <ThemeProvider>
+    <AppRouter />
+  </ThemeProvider>
+);
 
 export default App;
