cd0bda1b04a61aeb7ef7b9250b7a3317b9c447f1
cherkasovaa <alina.ch1202@gmail.com>
2025-06-05 20:40:39 +0300

    feat: add Logo component

---
+++ src/shared/ui/Logo/Logo.tsx
@@ -0,0 +1,17 @@
+import type { JSX } from 'react';
+
+import { Link } from 'react-router-dom';
+
+import { APP_PATHS } from '~/shared/config/router/paths';
+
+import logo from '../../assets/images/logo.png';
+
+export const Logo = (): JSX.Element => {
+  return (
+    <div className="h-16 w-16">
+      <Link to={APP_PATHS.HOME} aria-label="To home page">
+        <img src={logo} alt="Alina Cherkasova | Frontend Developer logo" />
+      </Link>
+    </div>
+  );
+};
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,4 +1,5 @@
 export { Button } from './Button/Button';
 export { ErrorAnimation } from './ErrorAnimation/ErrorAnimation';
 export { Heading } from './Heading/Heading';
+export { Logo } from './Logo/Logo';
 export { RouterLink } from './RouterLink/RouterLink';
