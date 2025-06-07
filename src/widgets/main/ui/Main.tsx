a6149a254cfc85bfc5718701a7b0e20349f9e3ed
cherkasovaa <alina.ch1202@gmail.com>
2025-06-07 10:16:13 +0300

    fix: stretch main component by height
    Ensure main always fills available vertical space between header and footer.

--- src/widgets/main/ui/Main.tsx
+++ src/widgets/main/ui/Main.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 
 export const Main = (): JSX.Element => {
   return (
-    <main className="container mx-auto h-full flex-1 px-4">
+    <main className="container mx-auto h-full px-4">
       <Outlet />
     </main>
   );
