83909d80520c927476e5db726b77bac841c687be
cherkasovaa <alina.ch1202@gmail.com>
2025-06-09 10:36:10 +0300

    refactor: improve bracket scaling

--- src/shared/ui/ButtonBracket/ButtonBracket.tsx
+++ src/shared/ui/ButtonBracket/ButtonBracket.tsx
@@ -5,18 +5,21 @@ import type { BracketProps } from './model/types';
 export const ButtonBracket = ({ side }: BracketProps): JSX.Element => {
   const isLeft = side === 'left';
 
-  const sideClasses = isLeft ? 'left-0 group-hover:-translate-x-0.5' : 'right-0 group-hover:translate-x-0.5';
-  const commonClasses = 'bg-accent absolute top-1/2';
+  const sideClasses = isLeft ? 'left-1 group-hover:-translate-x-1' : 'right-1 group-hover:translate-x-1';
+  const commonClasses = 'bg-accent absolute transition-all duration-300';
+  const edgeClasses = 'h-0.25 w-2 group-hover:w-3';
 
   return (
     <>
-      <span className={`${sideClasses} ${commonClasses} h-6 w-0.25 -translate-y-1/2 group-hover:h-6.5`}></span>
+      <span
+        className={`${sideClasses} ${commonClasses} top-1 h-[calc(100%-0.5rem)] w-0.25 group-hover:top-0 group-hover:h-full`}
+      ></span>
 
       {/* {Top part of bracket} */}
-      <span className={`${sideClasses} ${commonClasses} h-0.25 w-2 -translate-y-3 group-hover:-translate-y-3.5`}></span>
+      <span className={`${sideClasses} ${commonClasses} ${edgeClasses} top-1 group-hover:top-0`}></span>
 
       {/* {Bottom part of bracket} */}
-      <span className={`${sideClasses} ${commonClasses} h-0.25 w-2 translate-y-3 group-hover:translate-y-3.5`}></span>
+      <span className={`${sideClasses} ${commonClasses} ${edgeClasses} bottom-1 group-hover:bottom-0`}></span>
     </>
   );
 };
