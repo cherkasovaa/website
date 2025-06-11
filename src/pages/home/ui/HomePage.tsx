af5787055141e88f4df37a866e0007be43919a79
cherkasovaa <alina.ch1202@gmail.com>
2025-06-11 13:36:49 +0300

    feat: create HeroSection component

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,19 +1,12 @@
-import type { JSX } from 'react';
+import { type JSX } from 'react';
 
-import { HeroGreeting } from '~/features/hero-greeting';
-import { Terminal } from '~/features/terminal';
+import { HeroSection } from '~/widgets/hero-section';
 import { SkillsSection } from '~/widgets/skill-section';
 
 export const HomePage = (): JSX.Element => {
   return (
     <>
-      <section className="w-full">
-        <div className="grid w-full grid-cols-1 grid-rows-[minmax(max-content,1fr)] items-center gap-8 py-6 lg:grid-cols-2 lg:grid-rows-1">
-          <HeroGreeting />
-
-          <Terminal />
-        </div>
-      </section>
+      <HeroSection />
 
       <SkillsSection />
     </>
