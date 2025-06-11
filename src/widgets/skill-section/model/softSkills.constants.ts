c4ad12b80a0e20c2bf60eef3cef6f99dc84d00d7
cherkasovaa <alina.ch1202@gmail.com>
2025-06-11 15:12:19 +0300

    feat: update soft skills list

--- src/widgets/skill-section/model/softSkills.constants.ts
+++ src/widgets/skill-section/model/softSkills.constants.ts
@@ -1,9 +1,15 @@
 import type { Skill } from '~/shared/model/skill.types';
 
 export const softSkills: Skill[] = [
-  'I know how to work in a team, I have experience in communication with designers, managers and other developers',
+  'I know how to work in a team, I have experience in communication with managers and other developers',
+  'I am open to learning from others and value team collaboration',
+  'I am a non-confrontational person and always strive for constructive communication',
+
   'I am responsible about deadlines',
-  'I am demanding of the quality of my code, I follow the code-style',
-  'I have time management skills',
+  'I have excellent time management and self-organization skills',
+
+  'I am detail-oriented and maintain high code quality standards',
+
+  'I am open to feedback and always ready to improve my work',
   'I am motivated to improve my skills, constantly studying new technologies in my field of activity',
 ];
