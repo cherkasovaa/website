7503ee537469a4424f5905616cf47396927c0476
cherkasovaa <alina.ch1202@gmail.com>
2025-06-30 11:14:14 +0300

    refactor: optimize ProjectList with useSortedProjects and useMemo

--- src/features/project-list/ui/ProjectList.tsx
+++ src/features/project-list/ui/ProjectList.tsx
@@ -1,22 +1,21 @@
-import type { JSX } from 'react';
+import { type JSX, useMemo } from 'react';
 
 import { ProjectCard } from '~/entities/project-card';
+import { useSortedProjects } from '~/shared/lib/hooks/useSortedProjects';
 import { projects } from '~/shared/model/projects/constants';
 
 import type { ProjectListProps } from '../model/types';
 
 export const ProjectList = ({ limit = 3, showAll = false }: ProjectListProps): JSX.Element => {
-  const sortedProjects = [...projects]
-    .sort((prev, curr) => {
-      const prevDate = new Date(prev.date);
-      const currDate = new Date(curr.date);
-      return currDate.getTime() - prevDate.getTime();
-    })
-    .slice(0, showAll ? projects.length : limit);
+  const sortedProjects = useSortedProjects();
+  const latestProjects = useMemo(
+    () => sortedProjects.slice(0, showAll ? projects.length : limit),
+    [limit, showAll, sortedProjects],
+  );
 
   return (
     <ul className="portfolio-list">
-      {sortedProjects.map((project) => (
+      {latestProjects.map((project) => (
         <li key={project.id} className="portfolio-item mb-6">
           <ProjectCard project={project} />
         </li>
