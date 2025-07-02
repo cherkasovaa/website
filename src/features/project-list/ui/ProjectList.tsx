fd5d53eb4fb3aed8f69dc962151e6efbaa4987d6
cherkasovaa <alina.ch1202@gmail.com>
2025-07-02 10:43:55 +0300

    refactor: implement universal interface for lists

--- src/features/project-list/ui/ProjectList.tsx
+++ src/features/project-list/ui/ProjectList.tsx
@@ -2,11 +2,10 @@ import { type JSX, useMemo } from 'react';
 
 import { ProjectCard } from '~/entities/project-card';
 import { useSortedProjects } from '~/shared/lib/hooks/useSortedProjects';
+import type { ListProps } from '~/shared/model/list.types';
 import { projects } from '~/shared/model/projects/constants';
 
-import type { ProjectListProps } from '../model/types';
-
-export const ProjectList = ({ limit = 3, showAll = false }: ProjectListProps): JSX.Element => {
+export const ProjectList = ({ limit = 3, showAll = false }: ListProps): JSX.Element => {
   const sortedProjects = useSortedProjects();
   const latestProjects = useMemo(
     () => sortedProjects.slice(0, showAll ? projects.length : limit),
--- src/shared/model/list.types.ts
+++ src/shared/model/list.types.ts
@@ -1,4 +1,4 @@
-export interface ProjectListProps {
+export interface ListProps {
   limit?: number;
   showAll?: boolean;
 }
