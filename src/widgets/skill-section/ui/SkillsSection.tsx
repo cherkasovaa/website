e046c1c10151cdc4d0cf33dc467b85a4fb7e7adf
cherkasovaa <alina.ch1202@gmail.com>
2025-07-03 10:50:39 +0300

    feat: update SkillsSection with new components

--- src/widgets/skill-section/ui/SkillsSection.tsx
+++ src/widgets/skill-section/ui/SkillsSection.tsx
@@ -2,17 +2,35 @@ import { type JSX } from 'react';
 
 import { SkillList } from '~/entities/skill-list';
 import { APP_PATHS } from '~/shared/config/router/paths';
-import { Section } from '~/shared/ui';
+import { Heading, Section } from '~/shared/ui';
 
-import { getSkills } from '../lib/parseSkills';
 import { hardSkills } from '../model/hardSkills.constants';
 import { softSkills } from '../model/softSkills.constants';
 
+import { HardSkillCard } from './HardSkillCard';
+
 export const SkillsSection = (): JSX.Element => {
+  const SECTION_TITLE = 'Skills';
+
+  const hardSkillsGroups = hardSkills.reduce<Record<string, string[]>>((acc, skill) => {
+    const existingSkills = acc[skill.category] || [];
+    acc[skill.category] = [...existingSkills, ...skill.skills];
+    return acc;
+  }, {});
+
   return (
-    <Section id={APP_PATHS.SKILLS} className="grid grid-cols-1 gap-20 lg:grid-cols-2">
-      <SkillList title="Hard Skills" skills={getSkills(hardSkills)} />
-      <SkillList title="Soft Skills" skills={getSkills(softSkills)} />
+    <Section id={APP_PATHS.SKILLS}>
+      <Heading level={2}>{SECTION_TITLE}</Heading>
+
+      <div className="flex flex-col gap-8 lg:flex-row">
+        <div className="flex h-fit flex-wrap gap-4 lg:w-3/5">
+          {Object.entries(hardSkillsGroups).map((group) => (
+            <HardSkillCard key={group[0]} title={group[0]} skills={group[1]} />
+          ))}
+        </div>
+
+        <SkillList title="Soft Skills" skills={softSkills} className="lg:w-2/5" />
+      </div>
     </Section>
   );
 };
