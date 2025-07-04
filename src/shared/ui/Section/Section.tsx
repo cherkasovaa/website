876c174f28e91a1576c71549a11db8007a681819
cherkasovaa <alina.ch1202@gmail.com>
2025-07-04 19:41:40 +0300

    style: reduce section padding for mobile devices

--- src/shared/ui/Section/Section.tsx
+++ src/shared/ui/Section/Section.tsx
@@ -10,7 +10,7 @@ interface SectionProps {
 
 export const Section = ({ id, children, className }: SectionProps): JSX.Element => {
   return (
-    <section id={id} className={cn('flex flex-1 flex-col py-10 md:py-15', className)}>
+    <section id={id} className={cn('flex flex-1 flex-col py-6 md:py-15', className)}>
       {children}
     </section>
   );
