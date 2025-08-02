833024315defddc70135e3aee6f4585b9b75c3cd
cherkasovaa <alina.ch1202@gmail.com>
2025-08-02 20:18:43 +0300

    feat: remove info about my free time

--- src/widgets/about-section/model/constants.ts
+++ src/widgets/about-section/model/constants.ts
@@ -6,6 +6,4 @@ In 2018, an entire era of Flash games, once played by millions, was at risk of d
 
 At the same time, my curiosity about how the web works led me to freelancing and introduced me to the world of <span>SEO</span>. Years of experience taught me a crucial lesson: the importance of building products that <span>connect with their users</span>. This means that in my work, no detail is too small. My approach is to create a product that is <span>loved by</span> both <span>users and search engines</span>.
 
-Today, I am fully focused on <span>Frontend development</span> and am deeply immersed in the <span>modern tech stack</span>. I aim to bring value to businesses and users by creating <span>fast and beautiful web applications</span>. I am always open to new challenges and interesting projects where I can apply my <span>diverse experience</span>.
-
-In my free time, I visit museums or walk in parks. I cherish the time spent with my family and happily share my work chair with my two cats.`;
+Today, I am fully focused on <span>Frontend development</span> and am deeply immersed in the <span>modern tech stack</span>. I aim to bring value to businesses and users by creating <span>fast and beautiful web applications</span>. I am always open to new challenges and interesting projects where I can apply my <span>diverse experience</span>.`;
