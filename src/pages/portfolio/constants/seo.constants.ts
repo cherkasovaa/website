393d48ef78f23f40cf482012fadd2a68608b8dd6
cherkasovaa <alina.ch1202@gmail.com>
2025-09-06 16:58:47 +0300

    chore: clean up unused seo constants

--- src/pages/portfolio/constants/seo.constants.ts
+++
@@ -1,3 +0,0 @@
-export const SEO_PAGE_TITLE = 'Archive | Alina Cherkasova';
-export const SEO_PAGE_DESCRIPTION =
-  'An archive of completed projects with a short description, a stack of technologies and links to the source code.';
