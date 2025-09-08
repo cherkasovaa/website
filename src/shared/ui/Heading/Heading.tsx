cb43f1ec8e618d7a931facd3677761197c555eb9
cherkasovaa <alina.ch1202@gmail.com>
2025-09-08 12:48:07 +0300

    refactor: rename component directories from kebab-case to CamelCase

--- src/shared/ui/Heading/Heading.tsx
+++ src/shared/ui/Heading/Heading.tsx
@@ -1,10 +1,8 @@
 import type { JSX } from 'react';
 
 import { cn } from '@/shared/lib/utils/cn';
-
-import { levelClasses, tagMap } from './model/constants';
-
-import type { HeadingProps } from './model/types';
+import { levelClasses, tagMap } from '@/shared/ui/Heading/model/constants';
+import type { HeadingProps } from '@/shared/ui/heading/model/types';
 
 export const Heading = ({ level, children, className }: HeadingProps): JSX.Element => {
   const Tag = tagMap[level];
