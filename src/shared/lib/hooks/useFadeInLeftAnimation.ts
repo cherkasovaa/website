cd20ccdc8167e4af1baa464d272aec1c20898473
cherkasovaa <alina.ch1202@gmail.com>
2025-09-08 09:42:13 +0300

    refactor: update useFadeInLeftAnimation

--- src/shared/lib/hooks/useFadeInLeftAnimation.ts
+++ src/shared/lib/hooks/useFadeInLeftAnimation.ts
@@ -3,8 +3,8 @@ const DELAY_STEP = 0.15;
 
 export const useFadeInLeftAnimation = (index = 0) => {
   return {
-    initial: { opacity: 0, transform: 'translateX(20px)' },
-    animate: { opacity: 1, transform: 'translateX(0px)' },
+    initial: { opacity: 0, x: 20 },
+    animate: { opacity: 1, x: 0 },
     transition: { duration: DURATION, delay: DELAY_STEP * index, ease: 'easeInOut' },
   } as const;
 };
