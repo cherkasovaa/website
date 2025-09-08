06c538da44a77fd408adbebb28583e78378d9263
cherkasovaa <alina.ch1202@gmail.com>
2025-09-08 12:58:53 +0300

    fix: correct imports of types and constants in Heading

--- src/shared/ui/Heading/Heading.tsx
+++ src/shared/ui/Heading/Heading.tsx
@@ -1,8 +1,8 @@
 import type { JSX } from 'react';
 
 import { cn } from '@/shared/lib/utils/cn';
-import { levelClasses, tagMap } from '@/shared/ui/Heading/model/constants';
-import type { HeadingProps } from '@/shared/ui/heading/model/types';
+import { levelClasses, tagMap } from '@/shared/model/heading/model/constants';
+import type { HeadingProps } from '@/shared/model/heading/model/types';
 
 export const Heading = ({ level, children, className }: HeadingProps): JSX.Element => {
   const Tag = tagMap[level];
